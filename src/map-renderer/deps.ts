f6fbf41e115089f6118a9784c6eb2faba6de040c
timroberton <timroberton@gmail.com>
2023-11-29 20:02:51 -0500

    update deps

--- src/map-renderer/deps.ts
+++ src/map-renderer/deps.ts
@@ -2,5 +2,5 @@ export {
   type Canvas,
   type CanvasRenderingContext2D,
   ImageData,
-} from "https://deno.land/x/skia_canvas@0.5.4/mod.ts";
+} from "https://deno.land/x/skia_canvas@0.5.5/mod.ts";
 export { type ChromaStatic } from "npm:@types/chroma-js@2.4.0";
